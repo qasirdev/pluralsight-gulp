e9e2023937e37b37c96c0581a1816b743d3695c1
Qasir Mehmood <qasirdev@gmail.com>
2016-09-25 13:32:46 +0100

    05_07-Conditionally Displaying the Source Files

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,37 @@
 var gulp=require('gulp');
+var args=require('yargs').argv;
+
 var jshint=require('gulp-jshint');
 var jscs=require('gulp-jscs');
+var util=require('gulp-util');
+var gulpprint=require('gulp-print');
+var gulpif=require('gulp-if');
 
 gulp.task('vet',function(){
+    log('Analyzing source with JSHint and JSCS');
    return gulp
    .src([
         './src/**/*.js',
         './*.js'
     ])
+    .pipe(gulpif(args.verbose,gulpprint()))
     .pipe(jscs())
     .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish',{verbose:true}));
+    .pipe(jshint.reporter('jshint-stylish',{verbose:true})) /* return error with W117 code*/
+    .pipe(jshint.reporter('fail'));
 
 });
+
+///////////////////////
+function log(msg){
+    if(typeof(msg)==='object'){
+        for(var item in msg){
+            if(msg.hasOwnProperty(item)){
+                util.log(util.colors.blue(msg[item]));
+            }
+        }
+    }
+    else{
+        util.log(util.colors.blue(msg));
+    }
+}
