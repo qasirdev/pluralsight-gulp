44ae112be0401df34da07c9f15692caf692de457
Qasir Mehmood <qasirdev@gmail.com>
2016-09-25 18:57:40 +0100

    Handling Errors Plumber

--- gulp.config.js
+++ gulp.config.js
@@ -1,11 +1,16 @@
 module.exports=function(){
+    var client='./src/client/';
     var config={
+        temp:'./temp/',
+        /**
+         * Files Paths
+         */
         //all js for vet
         alljs:[
             './src/**/*.js',
             './*.js'
-        ]
-
+        ],
+        less: client + 'styles/styles.less'
     };
 
     return config;
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp=require('gulp');
 var args=require('yargs').argv;
+var del=require('del');
 var config=require('./gulp.config')(); //gulp.config.js
 
 var $=require('gulp-load-plugins')({lazy:true});
@@ -22,7 +23,40 @@ gulp.task('vet',function(){
 
 });
 
+//gulp.task('styles',['clean-styles'],function(){
+gulp.task('styles',function(){
+    log('compiling less --> CSS');
+    return gulp
+        .src(config.less) 
+        .pipe($.plumber())
+        .pipe($.less())
+        //.on('error',errorLogger)
+        .pipe($.autoprefixer({browsers:['last 2 version','> 5%']})) //last two version and greater than 5% in market
+        .pipe(gulp.dest(config.temp)) 
+});
+
+gulp.task('clean-styles',function(done){
+    var files=config.temp + '**/*.css';
+    //del(files);
+    clean(files,done);
+});
+
+gulp.task('less-watcher',function(){
+    gulp.watch([config.less],['styles']);
+});
 ///////////////////////
+function errorLogger(error){
+    log('*** Start of Error ***');
+    log(error);
+    log('*** End of Error ***');
+    this.emit('end');
+}
+
+function clean(path,done){
+    log('cleaning:' + $.util.colors.blue(path));
+    del(path,done);  // call back function don is in dle function
+}
+
 function log(msg){
     if(typeof(msg)==='object'){
         for(var item in msg){
